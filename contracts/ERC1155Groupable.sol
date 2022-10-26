919457a7d8784aec3e319d5e0a3bb1ba48f1fdc5
95decode <95decode@gmail.com>
2022-10-26 17:12:19 +0900

    Fix typo

--- contracts/ERC1155Groupable.sol
+++ contracts/ERC1155Groupable.sol
@@ -1,35 +1,32 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-//import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
- * @dev Extension of {ERC1155}
+ * @dev Extension of {ERC1155} that makes token groups with groupId and groupSubId
  */
 abstract contract ERC1155Groupable is ERC1155 {
-    //using Strings for uint256;
-
-    // Optional mapping for token URIs
+    // Optional mapping for group URIs
     mapping(uint256 => string) private _groupURIs;
 
-    function groupURI(uint256 groupId) public view virtual override returns (string memory) {
-        return = _groupURIs[groupId];
-    }
+    mapping(uint256 => mapping(uint256 => uint256)) private _groupIds;
 
     function _setGroupURI(uint256 groupId, string memory groupURI) internal virtual {
         _groupURIs[groupId] = groupURI;
     }
 
-    mapping(uint256 => mapping(uint256 => uint256)) private _groupIds;
-
     function _setGroup(uint256 tokenId, uint256 groupId, uint256 groupSubId) internal virtual {
         _groupIds[groupId][groupSubId] = tokenId;
-    };
+    }
 
-    function findIdWithGroup(uint256 groupId, uint256 groupSubId) public view virtual override returns (uint256) {
-        return _groupIds[groupId][groupSubId]
+    function findGroupURI(uint256 groupId) public view virtual returns (string memory) {
+        return _groupURIs[groupId];
+    }
+
+    function findIdWithGroup(uint256 groupId, uint256 groupSubId) public view virtual returns (uint256) {
+        return _groupIds[groupId][groupSubId];
     }
 
     function mintWithGroup(
@@ -42,7 +39,7 @@ abstract contract ERC1155Groupable is ERC1155 {
     ) public virtual {
         _mint(to, id, amount, data);
         _setGroup(id, groupId, groupSubId);
-    };
+    }
 
     function transferWithGroup(
         address from,
@@ -51,7 +48,7 @@ abstract contract ERC1155Groupable is ERC1155 {
         uint256 groupSubId,
         uint256 amount,
         bytes memory data
-    ) public virtual override {
+    ) public virtual {
         safeTransferFrom(
             from,
             to,
@@ -61,4 +58,7 @@ abstract contract ERC1155Groupable is ERC1155 {
         );
     }
 
+    // balanceOfWithGroup()
+
+    // 
 }

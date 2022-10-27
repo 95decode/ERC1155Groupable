608560b4e46f8b1b70591881998c7b775fee8ae7
95decode <95decode@gmail.com>
2022-10-27 19:07:45 +0900

    Apply group preset

--- contracts/ERC1155PresetGroup.sol
+++ contracts/ERC1155PresetGroup.sol
@@ -5,13 +5,18 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "./ERC1155Groupable.sol";
 
-contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Groupable {
+contract ERC1155PresetGroup is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Groupable, ERC1155Supply {
     constructor() ERC1155("") {}
 
     function setURI(string memory newuri) public onlyOwner {
         _setURI(newuri);
+    }
+
+    function setGroupURI(uint256 groupId, string memory uri) public onlyOwner {
+        _setGroupURI(groupId, uri);
     }
 
     function pause() public onlyOwner {
@@ -29,6 +34,13 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Groupabl
         _mint(account, id, amount, data);
     }
 
+    function setGroup(uint256 tokenId, uint256 groupId)
+        public
+        onlyOwner 
+    {
+        _setGroup(tokenId, groupId);
+    }
+
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         public
         onlyOwner
@@ -39,7 +51,7 @@ contract MyToken is ERC1155, Ownable, Pausable, ERC1155Burnable, ERC1155Groupabl
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
         internal
         whenNotPaused
-        override
+        override(ERC1155, ERC1155Groupable, ERC1155Supply)
     {
         super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }

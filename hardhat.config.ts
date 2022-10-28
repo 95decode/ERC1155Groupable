039185cbc32caab2047218c68575b3d6e65faaba
95decode <95decode@gmail.com>
2022-10-28 17:59:10 +0900

    Modify hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,50 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import "@nomicfoundation/hardhat-toolbox";
 
+task(
+  "accounts",
+  "Prints the list of accounts",
+  async (args, hre): Promise<void> => {
+      const accounts: SignerWithAddress[] = await hre.ethers.getSigners();
+      accounts.forEach((account: SignerWithAddress): void => {
+          console.log(account.address);
+      });
+  }
+);
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [{
+      version: "0.8.0",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    },{
+      version: "0.8.17",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+          details: {
+            yul: true,
+            yulDetails : {
+              stackAllocation : true
+            },
+          },
+        },
+      },
+    }],
+  },
 };
 
 export default config;
